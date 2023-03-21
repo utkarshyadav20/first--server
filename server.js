0d56f051f6b275a6c3e8c9fcad8ad4448f85c132
RunawayGOD <20022002utkarsh@gmail.com>
2023-03-21 18:24:35 +0530

    added a git ignore

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const http=require("http");
 
 const port=8081;
 
-http.createServer((request,response)=>{
-     response.writeHead(200,{"Content-Type":"text/html"});
-     response.write("<h1>This is my first server</h1>");
+http.createServer((request,response)=>{ 
+     response.writeHead(200,{"Content-Type":"html"});
+     response.write("<h1>This is my first server</h1>"); 
      response.end();   
 })
-.listen(port,()=>{
+.listen(port,()=>{ // callback function
      console.log(`Started the server on port no ${port}`);
 });
 
-// http://localhost:8081
+// http://localhost:8081  
