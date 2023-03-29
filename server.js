8340434bd78c9855ff3fe69f225a6cf16d0c95e6
RunawayGOD <20022002utkarsh@gmail.com>
2023-03-29 20:11:11 +0530

    added delete route

--- server.js
+++ server.js
@@ -15,13 +15,33 @@ app.get("/todo",(req,res)=>{
   //  res.send('Utkarsh');
 });
 app.post("/todo",(req,res)=>{
-  newTodo=req.body.item;
+  newTodo=req.body.item;     //body varible is automayically created and mapped to the JSON object containing item{advt of express}
   todo.push(newTodo);
   res.status(200).send({
-    message:"Task added succesfully"
+    message:"Task added succesfully"  //printing a json object 
   });
 
 });
+app.delete("/todo",(req,res)=>{
+  deleteThis=req.body.item;
+
+  // for(let i=0;i<todo.length;i++){
+  //   if(todo[i]==deleteThis){
+  //     todo.splice(i,1);
+  //   }
+  // }
+
+  todo.filter((element,index)=>{       //can also use find
+    if(element===deleteThis){
+      todo.splice(index,1);
+    }
+  })
+    
+  res.status(201).send({
+    message:"Deleted successfully"
+  });
+   
+})
 app.listen(port,()=>{
   console.log(`Server started on port ${port}`);
 });
