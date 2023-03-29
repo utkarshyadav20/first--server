916f8ae5aefaa93a88fc0e7cd0f43f7ba30bcda9
RunawayGOD <20022002utkarsh@gmail.com>
2023-03-29 13:28:23 +0530

    created get and post in todo route

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
+const express=require('express');
+const app=express(); //initializing for  using express methods
 
-
+app.use(express.json());
 
 
 const port=8081;     //making a local port
 const todo=["Water plants","do dsa","Do web dev"];
+app.get("/",(req,res)=>{
+  res.status(200).send (`<h1>first server expresss</h1>`);
+})
+app.get("/todo",(req,res)=>{
+    //callback
+  res.status(200).send(`<h1>${todo}</h1>`);
+  //  res.send('Utkarsh');
+});
+app.post("/todo",(req,res)=>{
+  newTodo=req.body.item;
+  todo.push(newTodo);
+  res.status(200).send({
+    message:"Task added succesfully"
+  });
+
+});
+app.listen(port,()=>{
+  console.log(`Server started on port ${port}`);
+});
